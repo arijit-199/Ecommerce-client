d0aa0ef6d5be20f4862f25edfe6397e9935c68ee
arijit1906 <arijit@dignexus.com>
2024-11-28 20:26:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router";
+import { Outlet } from "react-router-dom";
+import Homepage from './pages/Homepage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+
+function HomeLayout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<HomeLayout />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+
+
+
+
 
 export default App;
