db2388aa39035f912f3002e303c800713b60091e
arijit1906 <arijit@dignexus.com>
2024-11-28 20:43:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
 
 
 
@@ -21,8 +22,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index path='/' element={<Login />} />
         <Route element={<HomeLayout />}>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./../styles/styles.css";
+
+const Login = () => {
+  return (
+    <div className='loginContainer'>Login</div>
+  )
+}
+
+export default Login
